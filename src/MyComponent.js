f2887fdff0d44b323c6eb1cdb7abf1f6ed3b1f5f
Luong Duc Quynh <luongquynhduc4@gmail.com>
2024-09-26 14:45:13 +0700

    statestate2

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -9,12 +9,15 @@ class MyComponent extends React.Component{
     handleClick(){
         console.log("clivk")
     }
+    handleOver(event){
+        console.log(event.pageY)
+    }
     render(){
         return(
             <div>
                 My name is {this.state.name} i am from {this.state.address}
                 <button onClick={this.handleClick}>Click me</button>
-
+                <button onMouseOver={this.handleOver}>Hover Me</button>
             </div>
         )
     }
