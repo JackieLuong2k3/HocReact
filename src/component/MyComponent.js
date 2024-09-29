f9ebce0597b957b2b26eb7f7df2babf5ec3e488e
Luong Duc Quynh <luongquynhduc4@gmail.com>
2024-09-30 01:39:10 +0700

    add user

--- src/component/MyComponent.js
+++ src/component/MyComponent.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import UserInfor from './UserInfo';
+import AddUserInfor from './AddUserInfo';
 import List from './List'
 
 
 class MyComponent extends React.Component {
-    listUser =[
-        {id:1, name: "trang thoi", age:16},
-        {id:2,name: "jakie", age:18},
-        {id:3,name: "quynh", age:21},
-        
-    ]
+    state = {
+        listUser: [
+            { id: 1, name: "trang thoi", age: 16 },
+            { id: 2, name: "jakie", age: 18 },
+            { id: 3, name: "quynh", age: 21 },
+
+        ]
+    }
+    AddUserInfor = (user) => {
+        this.setState({
+            listUser: [user, ...this.state.listUser]
+        })
+    }
 
     render() {
         return (
             <div>
-                <UserInfor/>
-                <hr/>
-                <List listUser={this.listUser}></List>
+                <AddUserInfor AddUserInfor={this.AddUserInfor} />
+                <hr />
+                <List listUser={this.state.listUser} />
             </div>
-            
-            
+
+
         )
     }
 }
